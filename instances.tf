43fec107b796f6ffff1327ca696c96ce5936bc4b
Darryl Jordan <darryl.kn@kuikops.com>
2025-03-28 11:16:48 +0100

    My very first commit

---
+++ instances.tf
@@ -0,0 +1,70 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "km-kn-EC2-public-terra-tp-aws" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.km-kn-public-subnet-terra-tp-aws.id
+
+  user_data = <<-EOF
+            sudo dnf clean packages
+            sudo dnf clean all
+            sudo rpm --import https://rpm.nodesource.com/pubkey.gpg
+            
+            sudo dnf install -y https://rpm.nodesource.com/pub_20.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm
+            sudo dnf install -y nodejs      
+  EOF
+
+  tags = {
+    "Name" = "km-kn-EC2-public-terra-tp-aws"
+  }
+}
+
+resource "aws_ec2_instance_state" "km-kn-public-ec2-state-tp-aws" {
+  instance_id = aws_instance.km-kn-EC2-public-terra-tp-aws.id
+  state       = "running"
+}
+
+resource "aws_instance" "km-kn-EC2-private-terra-tp-aws" {
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.km-kn-private-subnet-terra-tp-aws.id
+  ami           = data.aws_ami.ubuntu.id
+  user_data     = <<-EOF
+            sudo dnf install -y https://dev.mysql.com/get/mysql80-community-release-el9-5.noarch.rpm
+            sudo dnf install -y mysql-community-server mysql-community-client
+            sudo systemctl start mysqld
+            sudo systemctl enable mysqld
+            sudo systemctl status mysqld
+  EOF
+
+  tags = {
+    "Name" = "km-kn-EC2-private-terra-tp-aws"
+  }
+}
+
+resource "aws_ec2_instance_state" "km-kn-private-ec2-state-tp-aws" {
+  instance_id = aws_instance.km-kn-EC2-private-terra-tp-aws.id
+  state       = "running"
+}
+
+resource "aws_security_group" "km-kn-security-group-terra-tp-aws" {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ main.tf
@@ -0,0 +1,83 @@
+data "aws_vpc" "existing_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["km-devops-session-1-2025-2"]
+  }
+}
+
+data "aws_internet_gateway" "existing_ig" {
+  filter {
+    name   = "attachment.vpc-id"
+    values = [data.aws_vpc.existing_vpc.id]
+  }
+}
+
+data "aws_eip" "existing_eip" {
+  public_ip = "54.78.238.172"
+}
+
+resource "aws_subnet" "km-kn-public-subnet-terra-tp-aws" {
+  vpc_id                  = data.aws_vpc.existing_vpc.id
+  cidr_block              = "50.20.10.32/28"
+  availability_zone       = "eu-west-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "km-kn-public-subnet-terra-tp-aws"
+  }
+}
+
+resource "aws_subnet" "km-kn-private-subnet-terra-tp-aws" {
+  vpc_id            = data.aws_vpc.existing_vpc.id
+  cidr_block        = "50.20.0.16/28"
+  availability_zone = "eu-west-1b"
+
+  tags = {
+    "Name" = "km-kn-private-subnet-terra-tp-aws"
+  }
+}
+
+resource "aws_route_table" "km-kn-public-route-table-terra-tp-aws" {
+  vpc_id = data.aws_vpc.existing_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = data.aws_internet_gateway.existing_ig.id
+  }
+  tags = {
+    "Name" = "km-kn-public-route-table-terra-tp-aws"
+  }
+
+  depends_on = [data.aws_internet_gateway.existing_ig]
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.km-kn-public-subnet-terra-tp-aws.id
+  route_table_id = aws_route_table.km-kn-public-route-table-terra-tp-aws.id
+}
+
+resource "aws_nat_gateway" "km-kn-NAT-gateway-terra-tp-aws" {
+  allocation_id     = data.aws_eip.existing_eip.id
+  subnet_id         = aws_subnet.km-kn-public-subnet-terra-tp-aws.id
+  connectivity_type = "public"
+  depends_on        = [data.aws_internet_gateway.existing_ig]
+  tags = {
+    "Name" = "km-kn-NAT-gateway-terra-tp-aws"
+  }
+}
+
+resource "aws_route_table" "km-kn-private-route-table-terra-tp-aws" {
+  vpc_id = data.aws_vpc.existing_vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.km-kn-NAT-gateway-terra-tp-aws.id
+  }
+  tags = {
+    "Name" = "km-kn-private-route-table-terra-tp-aws"
+  }
+  depends_on = [aws_nat_gateway.km-kn-NAT-gateway-terra-tp-aws]
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.km-kn-private-subnet-terra-tp-aws.id
+  route_table_id = aws_route_table.km-kn-private-route-table-terra-tp-aws.id
+}
